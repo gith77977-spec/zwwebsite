fc4b1030081dc3a98991641009eaf0a71bf1bf98
advpub7-netizen <gith77977@gmail.com>
2026-02-23 23:19:46 +0100

    trigger: force railway fresh redeploy now

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
     res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
     res.set('Pragma', 'no-cache');
     res.set('Expires', '0');
+    res.set('X-Redeploy-Trigger', Date.now().toString());
     next();
 });
 
